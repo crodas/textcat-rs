55fde009085bc97810ab66f1a50109df19eb77dc
Cesar Rodas <cesar@rodasm.com.py>
2022-01-05 13:46:37 -0300

    Preparing release 0.3.0
    * Update documentation and better struct naming
    * Added support for easier data conversion with into()
    * Update docs

--- src/bin/learn.rs
+++ src/bin/learn.rs
@@ -1,5 +1,5 @@
 use std::env;
-use textcat::storage::learn_from_directory;
+use textcat::category::learn_from_directory;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@
 //! Library to extract and categorize texts by ngrams.
 #![deny(missing_docs)]
 #![allow(warnings)]
+pub mod category;
 pub mod ngram;
-pub mod storage;
